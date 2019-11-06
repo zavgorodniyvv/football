a9ec0cad53a4eebbdf1c504cf22afb6bb1bf6931
Slava Zavhorodnii <zavgorodniy@gmail.com>
2019-11-06 14:46:11 +0200

    first commit - blank project

---
+++ src/main/java/name/nakonechnii/footballcompetitions/FootballcompetitionsApplication.java
@@ -0,0 +1,13 @@
+package name.nakonechnii.footballcompetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballcompetitionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballcompetitionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/name/nakonechnii/footballcompetitions/FootballcompetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package name.nakonechnii.footballcompetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballcompetitionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
